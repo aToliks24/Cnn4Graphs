4ebf615ddf5b06ee59686935fb1680d722fb4d31
toliks <shustera@post.bgu.ac.il>
2018-05-30 19:04:42 +0300

    Normalize graph

--- preprocess.py
+++ preprocess.py
@@ -13,22 +13,18 @@ def NeighAssemb(Graph,v,k):
     N=[[v]]
     ln=1
     colored_list=[]
-    vertexes=[v]
-    while ln<k:
-        vertexes,colored_list=get_next_bfs_layer(Graph, vertexes,colored_list)
-        ln+=len(vertexes)
+    while ln<k and len(N) < len(Graph):
+        vertexes,colored_list=get_next_bfs_layer(Graph, N[-1],colored_list)
         N.append(vertexes)
+        ln += len(N[-1])
     return N
-
-
-
-
 
 
 
 
 def get_next_bfs_layer(Graph, prev_layer,colored_list):
         stack_of_next_layer = []
+        prev_layer=list(prev_layer)
         colored_list=list(colored_list)
         while prev_layer !=[]:
             vertex=prev_layer[0]
@@ -39,16 +35,19 @@ def get_next_bfs_layer(Graph, prev_layer,colored_list):
         return stack_of_next_layer,colored_list
 
 
-def NormalizeGraph(Graph,U,v,labeling_func,k):
+def NormalizeGraph(Graph,U,labeling_func,k):
     sorted_layers=[]
     for bfs_layer in U:
         sorted_layers.extend(labeling_func(Graph,bfs_layer))
+    if len(sorted_layers)<k:
+        sorted_layers.extend(['d'+str(i) for i in range(k-len(sorted_layers))])
     return sorted_layers[:k]
 
 
 
 
-
+def random_order_labeling(Graph, bfs_layer):
+    return bfs_layer
 
 
 
@@ -78,5 +77,6 @@ def make_comparator(less_than):
 g=nx.read_graphml('Datasets/DD/DD_1.graphml')
 n=g.nodes()
 sorted_nodes_by_degree=sorted(g.degree(),key=lambda tup: tup[1],reverse=True)
-na=NeighAssemb(g,'n1',10)
+na=NeighAssemb(g,'n1',500)
+ng=NormalizeGraph(g, na, random_order_labeling, 500)
 p=0
