c939ab8955617892b2a4fc34beabca60a146c9a1
toliks <shustera@post.bgu.ac.il>
2018-07-03 16:16:54 +0300

    edge + vertex generator + preprocess works

--- data_generator.py
+++ data_generator.py
@@ -3,11 +3,14 @@ import keras
 import os
 import preprocess
 import networkx as nx
+from xml.dom import minidom
 import time
+import matplotlib.pyplot as plt
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, list_IDs, labels,data_dir, n_classes,batch_size=32, width=20,stride=1,k=5, shuffle=True):
+    def __init__(self, list_IDs, labels,data_dir, n_classes,batch_size=32, width=20,stride=1,k=5, shuffle=True,type='vertex'):
         'Initialization'
+        self.type = type
         self.width=width
         self.stride=stride
         self.k=k
@@ -20,6 +23,10 @@ class DataGenerator(keras.utils.Sequence):
         self.shuffle = shuffle
         self.data_dir = data_dir
         self.on_epoch_end()
+        if type not in ['edge','vertex','comb']:
+            raise ValueError('type should be in: [\'vertex\',\'edge\',\'comb\']')
+
+
 
 
     def __len__(self):
@@ -53,24 +60,52 @@ class DataGenerator(keras.utils.Sequence):
     def __data_generation(self, list_IDs_temp):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
-        X_list = []
+        X_vertex_list=[]
+        X_edge_list = []
+        model_input=[]
         y = []
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
             curr_path=self.data_dir + ID
-            if os.path.exists(curr_path+ '.npz'):
-                X_list.append(np.array(np.load(curr_path+'.npz')['arr_0']))
+            if os.path.exists(curr_path+ '_vertex.npz' ) and  self.type == 'vertex':
+                X_vertex_list.append(np.array(np.load(curr_path + '_vertex.npz')['arr_0']))
+            if os.path.exists(curr_path + '_edge.npz') and self.type == 'edge':
+                X_edge_list.append(np.array(np.load(curr_path+'_edge.npz')['arr_0']))
             else:
                 g=nx.read_graphml(curr_path)
-                pp= preprocess.SelNodeSeq(g,preprocess.canonical_subgraph,stride=self.stride,width=self.width,k=self.k)
-                np.savez_compressed(curr_path,pp)
-                X_list.append(np.array(pp))
-
+                pp1=None
+                pp2 = None
+                if self.type=='vertex':
+                    pp1 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride, width=self.width,k=self.k)
+                    np.savez_compressed(curr_path,pp1)
+                    X_vertex_list.append(np.array(pp1))
+                if self.type=='edge':
+                    xmldoc = minidom.parse(curr_path)
+                    itemlist = xmldoc.getElementsByTagName('edge')
+                    lg = nx.line_graph(g)
+                    for item in itemlist:
+                        value=float(item.firstChild.TEXT_NODE)
+                        source=item.attributes['source'].value
+                        target=item.attributes['target'].value
+                        if (source, target) not in lg.nodes.data():
+                            lg.add_node((source, target))
+                        lg.nodes.data()[(source, target)].update({'label':value})
+                    [x[1].update({'value':0}) for x in lg.nodes.data() if len(x[1]) == 0]
+                    pp2 = preprocess.SelNodeSeq(lg, preprocess.canonical_subgraph, stride=self.stride, width=self.width,k=self.k)
+                    np.savez_compressed(curr_path, pp2)
+                    X_edge_list.append(np.array(pp2))
             # Store class
-            y.append( self.labels[ID])
-        y=np.array(y)
-        X=np.vstack(X_list)
+            y.append(self.labels[ID])
+        one_hot_y=keras.utils.to_categorical(y, num_classes=self.n_classes)
+        if self.type=='vertex':
+            model_input = np.expand_dims(np.vstack(X_vertex_list), axis=2),one_hot_y
+        elif self.type=='edge':
+            model_input=np.expand_dims(np.vstack(X_edge_list),axis=2),one_hot_y
 
-        return np.expand_dims( X,axis=2), keras.utils.to_categorical(y, num_classes=self.n_classes)
+        return model_input
+
+
+class two_input_generator_bilder(object):
+    pass
--- experiments.py
+++ experiments.py
@@ -1,12 +1,14 @@
-from keras.models import Sequential
-from keras.layers import Conv1D,Flatten,Dense,Dropout
+from keras.models import Sequential,Model
+from keras.layers import Conv1D,Flatten,Dense,Dropout,LSTM,Input,Concatenate
+from keras.callbacks import TensorBoard
 import preprocess
 import data_generator
 import time
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 np.random.seed(7)
-
+import networkx as nx
 
 '''
 def create_2Dcnn(k,width, num_of_classes,stride):  # doesn't work
@@ -21,7 +23,7 @@ def create_2Dcnn(k,width, num_of_classes,stride):  # doesn't work
 
 
 
-def prepare_paths(dataset_dict):
+def prepare_paths(dataset_dict,overwrite=True):
     data_dir = dataset_dict['path']
     f_data = dataset_dict['data']
     f_labels = dataset_dict['labels']
@@ -36,23 +38,39 @@ def prepare_paths(dataset_dict):
                 labels_to_index[l] = count
                 count += 1
         labels = {data[i]: labels_to_index[labels[i]] for i in range(len(labels))}
-
-    filelist = [f for f in os.listdir(data_dir) if f.endswith(".npz")]
-    for f in filelist:
-        os.remove(os.path.join(data_dir, f))
+    if overwrite:
+        filelist = [f for f in os.listdir(data_dir) if f.endswith(".npz")]
+        for f in filelist:
+            os.remove(os.path.join(data_dir, f))
     return data, labels
 
 def create_1Dcnn(k,width, num_of_classes):
     model = Sequential()
-    model.add(Conv1D(16, kernel_size=k, strides=width, activation='linear',input_shape=(k*width, 1)))
-    model.add(Conv1D(8, kernel_size=10, strides=1, activation='linear',padding='same'))
+    model.add(Conv1D(16, kernel_size=k, strides=width, activation='relu',input_shape=(k*width, 1)))
+    model.add(Conv1D(8, kernel_size=10, strides=1, activation='relu',padding='same'))
     model.add(Flatten())
-    model.add(Dense(128,activation='linear'))
+    model.add(Dense(128,activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(num_of_classes, activation='softmax'))
-    model.compile(optimizer='adam', loss='categorical_crossentropy')  # optimizer, metrics
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])  # optimizer, metrics
     return model
 
+def create_1DdoubleCnn(k,w1,w2, num_of_classes):
+    I1=Input((k*w1,1),name='vertex_input')
+    C1=Conv1D(16, kernel_size=k, strides=w1, activation='relu',input_shape=(k*w1, 1))(I1)
+    M1=Model(I1,C1)
+    I2 = Input((k * w2, 1),name='edge_input')
+    C2=Conv1D(16, kernel_size=k, strides=w2, activation='relu',input_shape=(k*w2, 1))(I2)
+    M2=Model(I2,C2)
+    conc = Concatenate()([M1.output, M2.output])
+    C3=Conv1D(8, kernel_size=10, strides=1, activation='relu',padding='same')(conc)
+    F1=Flatten()(C3)
+    Dense1=Dense(128,activation='relu')(F1)
+    Drop1=Dropout(0.5)(Dense1)
+    SM=Dense(num_of_classes, activation='softmax')(Drop1)
+    ModelAll=Model([I1,I2],SM)
+    ModelAll.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    return ModelAll
 
 Datasets={'enzymes':{'path':'Datasets/enzymes/',
                     'labels':'Datasets/enzymes/enzymes.label',
@@ -82,31 +100,29 @@ NCI1='NCI1'
 
 
 
-
-
-
 curr_ds_name=mutag
-
-
-k=20
-width=5
-n_epochs=30
-test_percent=0.2
-data, labels=prepare_paths(Datasets[curr_ds_name])
+k=10
+width=30
+n_epochs=5
+test_percent=0.1
+type='edge' #  'vertex' or 'edge' or 'comb'
+data, labels=prepare_paths(Datasets[curr_ds_name],overwrite=True)
 
 
 num_of_classes=len(set(labels.values()))
 rands = np.random.random(len(data))
 m=create_1Dcnn(k,width,num_of_classes)
+#m=create_1DdoubleCnn(k,width,width,num_of_classes)
 X_train=data[rands<=(1-test_percent)]
 X_test=data[rands>test_percent]
 
 
-dg_train=data_generator.DataGenerator(X_train,labels,Datasets[curr_ds_name]['path'],len(set(labels.values())),width=width,k=k)
-dg_test=data_generator.DataGenerator(X_test,labels,Datasets[curr_ds_name]['path'],len(set(labels.values())),width=width,k=k)
+dg_train=data_generator.DataGenerator(X_train,labels,Datasets[curr_ds_name]['path'],len(set(labels.values())),width=width,k=k,type=type)
+dg_test=data_generator.DataGenerator(X_test,labels,Datasets[curr_ds_name]['path'],len(set(labels.values())),width=width,k=k,type=type)
 data_test,labels_test=dg_test.getallitems()
-m.fit_generator(dg_train,epochs=n_epochs,verbose=2)
-y_pred=m.predict_classes(data_test)
-y_true=[ np.where(r==1)[0][0] for r in labels_test ]
-acc=np.sum([i==j for i,j in zip(y_pred,y_true)])/len(labels_test)
-print('The accuracy is : %.4f'%acc)
+m.fit_generator(dg_train,epochs=n_epochs,verbose=2,validation_data=dg_test.getallitems(),callbacks=[TensorBoard('TensorBoardDir/')])
+
+#y_pred=m.predict_classes(data_test)
+#y_true=[ np.where(r==1)[0][0] for r in labels_test ]
+#acc=np.sum([i==j for i,j in zip(y_pred,y_true)])/len(labels_test)
+#print('The accuracy is : %.4f'%acc)
