331bf8a830a3d1389f30eacd98b752059105131e
Alon Freund <alonf@il.ibm.com>
2018-07-27 12:08:37 +0300

    code reformat

--- data_generator.py
+++ data_generator.py
@@ -5,20 +5,24 @@ import preprocess
 import networkx as nx
 from xml.dom import minidom
 import time
+
+
 # import matplotlib.pyplot as plt
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, list_IDs, labels, data_dir, n_classes, batch_size=32, width=20, stride=1, k=5, shuffle=True, mode='vertex', seed=7):
+
+    def __init__(self, list_IDs, labels, data_dir, n_classes, batch_size=32, width=20, stride=1, k=5, shuffle=True,
+                 mode='vertex', seed=7):
         'Initialization'
-        #todo: support multiple width for combined
+        # todo: support multiple width for combined
         self.mode = mode
-        self.width=width
-        self.stride=stride
-        self.k=k
+        self.width = width
+        self.stride = stride
+        self.k = k
         channels = 1
-        if mode=='vertex_channels':
-            channels=4
-        self.dim = (k*sum(width),channels)
+        if mode == 'vertex_channels':
+            channels = 4
+        self.dim = (k * sum(width), channels)
         self.batch_size = batch_size
         self.labels = labels
         self.list_IDs = list_IDs
@@ -38,7 +42,7 @@ class DataGenerator(keras.utils.Sequence):
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
         # Find list of IDs
         list_IDs_temp = [self.list_IDs[k] for k in indexes]
@@ -50,7 +54,7 @@ class DataGenerator(keras.utils.Sequence):
 
     def getallitems(self):
         X, y = self.__data_generation(self.list_IDs)
-        return X,y
+        return X, y
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
@@ -60,69 +64,74 @@ class DataGenerator(keras.utils.Sequence):
         time.sleep(0.5)
 
     def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
-        X_vertex_list=[]
+        X_vertex_list = []
         X_edge_list = []
-        X_vertex_channel_list=[]
-        model_input=[]
+        X_vertex_channel_list = []
+        model_input = []
         y = []
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
-            curr_path=self.data_dir + ID
-            if os.path.exists(curr_path+ '_vertex.npz' ) and  self.mode == 'vertex':
+            curr_path = self.data_dir + ID
+            if os.path.exists(curr_path + '_vertex.npz') and self.mode == 'vertex':
                 X_vertex_list.append(np.array(np.load(curr_path + '_vertex.npz')['arr_0']))
             elif os.path.exists(curr_path + '_edge.npz') and self.mode == 'edge':
-                X_edge_list.append(np.array(np.load(curr_path+'_edge.npz')['arr_0']))
+                X_edge_list.append(np.array(np.load(curr_path + '_edge.npz')['arr_0']))
             else:
-                if self.mode=='edge':
+                if self.mode == 'edge':
                     edge_width = self.width[0]
-                elif self.mode in ['vertex','vertex_channels']:
+                elif self.mode in ['vertex', 'vertex_channels']:
                     vertex_width = self.width[0]
                 else:
                     vertex_width = self.width[0]
                     edge_width = self.width[1]
-                if self.mode in ['vertex','vertex_channels','comb']:
+                if self.mode in ['vertex', 'vertex_channels', 'comb']:
                     g = nx.read_graphml(curr_path)
-                    pp1 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride, width=vertex_width,k=self.k)
-                    np.savez_compressed(curr_path+'_vertex',pp1)
+                    pp1 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride,
+                                                width=vertex_width, k=self.k)
+                    np.savez_compressed(curr_path + '_vertex', pp1)
                     X_vertex_list.append(np.array(pp1))
                     if self.mode == 'vertex_channels':
                         cs = nx.closeness_centrality(g)
                         nx.set_node_attributes(g, cs, 'label')
-                        pp1_1 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride, width=vertex_width,k=self.k)
+                        pp1_1 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride,
+                                                      width=vertex_width, k=self.k)
 
                         bs = nx.betweenness_centrality(g)
                         nx.set_node_attributes(g, bs, 'label')
-                        pp1_2 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride, width=vertex_width,k=self.k)
+                        pp1_2 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride,
+                                                      width=vertex_width, k=self.k)
 
                         ds = nx.degree_centrality(g)
                         nx.set_node_attributes(g, ds, 'label')
-                        pp1_3 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride, width=vertex_width,k=self.k)
+                        pp1_3 = preprocess.SelNodeSeq(g, preprocess.canonical_subgraph, stride=self.stride,
+                                                      width=vertex_width, k=self.k)
 
-
-                        voodoo_python= [np.array(x) for x in [ pp1,pp1_1,pp1_2,pp1_3]]
-                        X_vertex_channel_list.append(np.stack(voodoo_python,axis=1))
-                if self.mode== 'edge' or self.mode== 'comb':
+                        voodoo_python = [np.array(x) for x in [pp1, pp1_1, pp1_2, pp1_3]]
+                        X_vertex_channel_list.append(np.stack(voodoo_python, axis=1))
+                if self.mode == 'edge' or self.mode == 'comb':
                     g = nx.read_graphml(curr_path)
                     lg = self.vertexes_to_edges_graph(curr_path, g)
-                    pp2 = preprocess.SelNodeSeq(lg, preprocess.canonical_subgraph, stride=self.stride, width=edge_width,k=self.k)
-                    np.savez_compressed(curr_path+ '_edge', pp2)
+                    pp2 = preprocess.SelNodeSeq(lg, preprocess.canonical_subgraph, stride=self.stride, width=edge_width,
+                                                k=self.k)
+                    np.savez_compressed(curr_path + '_edge', pp2)
                     X_edge_list.append(np.array(pp2))
             # Store class
             y.append(self.labels[ID])
-        one_hot_y=keras.utils.to_categorical(y, num_classes=self.n_classes)
-        if self.mode== 'vertex':
+        one_hot_y = keras.utils.to_categorical(y, num_classes=self.n_classes)
+        if self.mode == 'vertex':
             model_input = np.expand_dims(np.vstack(X_vertex_list), axis=2)
-        elif self.mode== 'edge':
-            model_input=np.expand_dims(np.vstack(X_edge_list),axis=2)
-        elif self.mode== 'comb':
-            model_input={'vertex_input':np.expand_dims(np.vstack(X_vertex_list), axis=2),'edge_input':np.expand_dims(np.vstack(X_edge_list),axis=2)}
-        elif self.mode== 'vertex_channels':
-            model_input=np.asanyarray(X_vertex_channel_list)
-        return model_input,one_hot_y
+        elif self.mode == 'edge':
+            model_input = np.expand_dims(np.vstack(X_edge_list), axis=2)
+        elif self.mode == 'comb':
+            model_input = {'vertex_input': np.expand_dims(np.vstack(X_vertex_list), axis=2),
+                           'edge_input': np.expand_dims(np.vstack(X_edge_list), axis=2)}
+        elif self.mode == 'vertex_channels':
+            model_input = np.asanyarray(X_vertex_channel_list)
+        return model_input, one_hot_y
 
     def vertexes_to_edges_graph(self, curr_path, v_graph):
         xmldoc = minidom.parse(curr_path)
@@ -137,5 +146,3 @@ class DataGenerator(keras.utils.Sequence):
             e_graph.nodes.data()[(source, target)].update({'label': value})
         [x[1].update({'value': 0}) for x in e_graph.nodes.data() if len(x[1]) == 0]
         return e_graph
-
-
--- preprocess.py
+++ preprocess.py
@@ -32,7 +32,7 @@ def canonical_subgraph(G, nodes):
     nodes_lbls = OrderedDict()
     nodes_lbls_prev = OrderedDict()
     for v in sub_G.nodes():
-        nodes_lbls_prev[v] =[None, None]
+        nodes_lbls_prev[v] = [None, None]
         nodes_lbls[v] = [str(1).zfill(n_dig), None]
 
     while not is_canonical(nodes_lbls_prev, nodes_lbls):
@@ -44,7 +44,7 @@ def canonical_subgraph(G, nodes):
             nodes_lbls[v][1] = ''.join(nbrs_l)
         nodes_lbls = OrderedDict(sorted(nodes_lbls.items(), key=lambda t: (t[1][0], t[1][1])))
 
-        nodes_lbls_prev = copy.deepcopy(nodes_lbls) # do deep copy
+        nodes_lbls_prev = copy.deepcopy(nodes_lbls)  # do deep copy
 
         rank = 0
         prev_l = None
@@ -53,25 +53,25 @@ def canonical_subgraph(G, nodes):
                 prev_l = nodes_lbls[v].copy()
                 rank += 1
             nodes_lbls[v][0] = str(rank).zfill(n_dig)
-    res=list(nodes_lbls.keys())
+    res = list(nodes_lbls.keys())
     res.reverse()
     return res
 
 
 def SelNodeSeq(Graph, canonization_func, stride, width, k):
     v_sorted = sorted(Graph.degree(), key=lambda tup: tup[1], reverse=True)[:width]
-    i=0
-    j=0
-    input_channel_nodes=[]
-    input_channe=[]
-    while j< width:
+    i = 0
+    j = 0
+    input_channel_nodes = []
+    input_channe = []
+    while j < width:
         if i < len(v_sorted):
-            f=ReceptiveField(Graph, canonization_func, v_sorted[i][0], k)
+            f = ReceptiveField(Graph, canonization_func, v_sorted[i][0], k)
         else:
-            f=ZeroReceptiveField(k)
+            f = ZeroReceptiveField(k)
         input_channel_nodes.extend(f)
-        i+=stride
-        j+=1
+        i += stride
+        j += 1
     for icn in input_channel_nodes:
         if icn in Graph:
             input_channe.append(list(Graph.nodes.data()[icn].values())[0])
@@ -80,62 +80,60 @@ def SelNodeSeq(Graph, canonization_func, stride, width, k):
     return input_channe
 
 
-
-
 def ReceptiveField(Graph, canonization_func, v, k):
-    N=NeighAssemb(Graph,v,k)
-    G=NormalizeGraph(Graph, N, canonization_func, k)
+    N = NeighAssemb(Graph, v, k)
+    G = NormalizeGraph(Graph, N, canonization_func, k)
     return G
 
 
 def ZeroReceptiveField(k):
-    return [0]*k
+    return [0] * k
 
 
-def NeighAssemb(Graph,v,k):
-    N=[[v]]
-    ln=1
-    colored_list=[]
-    while ln<k and len(N) < len(Graph):
-        vertexes,colored_list=get_next_bfs_layer(Graph, N[-1],colored_list)
+def NeighAssemb(Graph, v, k):
+    N = [[v]]
+    ln = 1
+    colored_list = []
+    while ln < k and len(N) < len(Graph):
+        vertexes, colored_list = get_next_bfs_layer(Graph, N[-1], colored_list)
         N.append(vertexes)
         ln += len(N[-1])
     return N
 
+
 def test_graph_cannonization():
     tmp = nx.Graph()
-    tmp.add_edge(1,3)
-    tmp.add_edge(2,3)
-    tmp.add_edge(3,4)
-    tmp.add_edge(4,5)
-    tmp.add_edge(5,6)
-    tmp.add_edge(6,3)
+    tmp.add_edge(1, 3)
+    tmp.add_edge(2, 3)
+    tmp.add_edge(3, 4)
+    tmp.add_edge(4, 5)
+    tmp.add_edge(5, 6)
+    tmp.add_edge(6, 3)
     print(canonical_subgraph(tmp, tmp.nodes()))
 
 
-def get_next_bfs_layer(Graph, prev_layer,colored_list):
-        stack_of_next_layer = []
-        prev_layer=list(prev_layer)
-        colored_list=list(colored_list)
-        while prev_layer !=[]:
-            vertex=prev_layer[0]
-            prev_layer.remove(vertex)
-            colored_list.append(vertex)
-            stack_of_next_layer.extend( list(Graph.neighbors(vertex)))
-        stack_of_next_layer=list(set(stack_of_next_layer).difference(set(colored_list)))
-        return stack_of_next_layer,colored_list
+def get_next_bfs_layer(Graph, prev_layer, colored_list):
+    stack_of_next_layer = []
+    prev_layer = list(prev_layer)
+    colored_list = list(colored_list)
+    while prev_layer != []:
+        vertex = prev_layer[0]
+        prev_layer.remove(vertex)
+        colored_list.append(vertex)
+        stack_of_next_layer.extend(list(Graph.neighbors(vertex)))
+    stack_of_next_layer = list(set(stack_of_next_layer).difference(set(colored_list)))
+    return stack_of_next_layer, colored_list
 
 
 def NormalizeGraph(Graph, U, canonization_func, k):
-    sorted_layers=[]
-    all_nodes=[]
+    sorted_layers = []
+    all_nodes = []
     for bfs_layer in U:
         sorted_layers.extend(canonization_func(Graph, bfs_layer))
         all_nodes.extend(bfs_layer)
-    if len(sorted_layers)<k:
-        sorted_layers.extend(['d'+str(i) for i in range(k-len(sorted_layers))])
+    if len(sorted_layers) < k:
+        sorted_layers.extend(['d' + str(i) for i in range(k - len(sorted_layers))])
     return sorted_layers[:k]
-
 
 
 '''
@@ -147,4 +145,3 @@ g=nx.read_graphml('Datasets/DD/DD_1.graphml')
 ns=SelNodeSeq(g,random_order_labeling,stride=3,width=10,k=100)
 p=0
 '''
-
