7ef8866d6b5ae5942345ae8cf534b2f99cd34216
toliks <shustera@post.bgu.ac.il>
2018-07-28 18:22:29 +0300

    preprocess documenmtation

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,136 @@
-import networkx as nx
 from collections import OrderedDict
 import copy
-import numpy as np
 
 
-def is_canonical(nodes_lbls_prev, nodes_lbls):
-    return all([nodes_lbls[k][0] == nodes_lbls_prev[k][0] for k in nodes_lbls.keys()])
+def SelNodeSeq(Graph, canonization_func, stride, width, k):
+    """
+    Description:
+    Node Sequence Selection algorithm, create features from graph.
+    input:
+    Graph – the raw graph.
+    canonization_func – function applying partial order on graph.
+    stride - Relative distance between the selected sequences.
+    width - size of the receptive field.
+    k - number fof receptive fields.
+    Output:
+    input_channel - a feature vector for machine learning application.
+    """
+    v_sorted = sorted(Graph.degree(), key=lambda tup: tup[1], reverse=True)[:width]
+    i = 0
+    j = 0
+    input_channel_nodes = []
+    input_channel = []
+    while j < width:
+        if i < len(v_sorted):
+            f = ReceptiveField(Graph, canonization_func, v_sorted[i][0], k)
+        else:
+            f = ZeroReceptiveField(k)
+        input_channel_nodes.extend(f)
+        i += stride
+        j += 1
+    for icn in input_channel_nodes:
+        if icn in Graph:
+            input_channel.append(list(Graph.nodes.data()[icn].values())[0])
+        else:
+            input_channel.append(0)
+    return input_channel
+
+
+def ReceptiveField(Graph, canonization_func, v, k):
+    """
+    Description:
+    Creation a Receptive field vector starts with root node v in Graph.
+    input:
+    Graph – the raw graph.
+    canonization_func – function applying partial order on graph.
+    v - the root node of the receptive field.
+    k - number of receptive fields
+    Output:
+    G - vector of one canonized receptive field
+    """
+    N = NeighAssemb(Graph, v, k)
+    G = NormalizeGraph(Graph, N, canonization_func, k)
+    return G
+
+
+def ZeroReceptiveField(k):
+    """
+    Description:
+    Padding to vector
+    input:
+    k - size of the vector
+    Output:
+    Vector of zeros
+    """
+    return [0] * k
+
+
+def NeighAssemb(Graph, v, k):
+    """
+    Description:
+    Building the neighborhood of node v.
+    input:
+    Graph – the raw graph.
+    v - the root node of the receptive field.
+    k - number of receptive fields
+    Output:
+    N - vertexes list of the neighborhood.
+    """
+    N = [[v]]
+    ln = 1
+    colored_list = []
+    while ln < k and len(N) < len(Graph):
+        vertexes, colored_list = get_next_bfs_layer(Graph, N[-1], colored_list)
+        N.append(vertexes)
+        ln += len(N[-1])
+    return N
+
+
+def get_next_bfs_layer(Graph, prev_layer, colored_list):
+    """
+    Description:
+    Gets the next layer of the neigborhood.
+    input:
+    Graph – the raw graph.
+    prev_layer - the last expanded layer of vertex neighbors.
+    colored_list - the list of already members vertexes.
+    Output:
+    stack_of_next_layer - the next layer of bfs in the neighborbood.
+    colored_list - new list of members vertexes.
+    """
+    stack_of_next_layer = []
+    prev_layer = list(prev_layer)
+    colored_list = list(colored_list)
+    while prev_layer != []:
+        vertex = prev_layer[0]
+        prev_layer.remove(vertex)
+        colored_list.append(vertex)
+        stack_of_next_layer.extend(list(Graph.neighbors(vertex)))
+    stack_of_next_layer = list(set(stack_of_next_layer).difference(set(colored_list)))
+    return stack_of_next_layer, colored_list
+
+
+def NormalizeGraph(Graph, U, canonization_func, k):
+    """
+    Description:
+    orders a receptive field in U neighborhood layers.
+    input:
+    Graph – the raw graph.
+    U - list of the assembeled neighborhood by layer.
+    canonization_func – function applying partial order on graph.
+    k - number of receptive fields
+    Output:
+    sorted vector of receptive field.
+    """
+    sorted_layers = []
+    all_nodes = []
+    for bfs_layer in U:
+        sorted_layers.extend(canonization_func(Graph, bfs_layer))
+        all_nodes.extend(bfs_layer)
+    if len(sorted_layers) < k:
+        sorted_layers.extend(['d' + str(i) for i in range(k - len(sorted_layers))])
+    return sorted_layers[:k]
+
 
 
 def canonical_subgraph(G, nodes):
@@ -57,91 +182,5 @@ def canonical_subgraph(G, nodes):
     res.reverse()
     return res
 
-
-def SelNodeSeq(Graph, canonization_func, stride, width, k):
-    v_sorted = sorted(Graph.degree(), key=lambda tup: tup[1], reverse=True)[:width]
-    i = 0
-    j = 0
-    input_channel_nodes = []
-    input_channe = []
-    while j < width:
-        if i < len(v_sorted):
-            f = ReceptiveField(Graph, canonization_func, v_sorted[i][0], k)
-        else:
-            f = ZeroReceptiveField(k)
-        input_channel_nodes.extend(f)
-        i += stride
-        j += 1
-    for icn in input_channel_nodes:
-        if icn in Graph:
-            input_channe.append(list(Graph.nodes.data()[icn].values())[0])
-        else:
-            input_channe.append(0)
-    return input_channe
-
-
-def ReceptiveField(Graph, canonization_func, v, k):
-    N = NeighAssemb(Graph, v, k)
-    G = NormalizeGraph(Graph, N, canonization_func, k)
-    return G
-
-
-def ZeroReceptiveField(k):
-    return [0] * k
-
-
-def NeighAssemb(Graph, v, k):
-    N = [[v]]
-    ln = 1
-    colored_list = []
-    while ln < k and len(N) < len(Graph):
-        vertexes, colored_list = get_next_bfs_layer(Graph, N[-1], colored_list)
-        N.append(vertexes)
-        ln += len(N[-1])
-    return N
-
-
-def test_graph_cannonization():
-    tmp = nx.Graph()
-    tmp.add_edge(1, 3)
-    tmp.add_edge(2, 3)
-    tmp.add_edge(3, 4)
-    tmp.add_edge(4, 5)
-    tmp.add_edge(5, 6)
-    tmp.add_edge(6, 3)
-    print(canonical_subgraph(tmp, tmp.nodes()))
-
-
-def get_next_bfs_layer(Graph, prev_layer, colored_list):
-    stack_of_next_layer = []
-    prev_layer = list(prev_layer)
-    colored_list = list(colored_list)
-    while prev_layer != []:
-        vertex = prev_layer[0]
-        prev_layer.remove(vertex)
-        colored_list.append(vertex)
-        stack_of_next_layer.extend(list(Graph.neighbors(vertex)))
-    stack_of_next_layer = list(set(stack_of_next_layer).difference(set(colored_list)))
-    return stack_of_next_layer, colored_list
-
-
-def NormalizeGraph(Graph, U, canonization_func, k):
-    sorted_layers = []
-    all_nodes = []
-    for bfs_layer in U:
-        sorted_layers.extend(canonization_func(Graph, bfs_layer))
-        all_nodes.extend(bfs_layer)
-    if len(sorted_layers) < k:
-        sorted_layers.extend(['d' + str(i) for i in range(k - len(sorted_layers))])
-    return sorted_layers[:k]
-
-
-'''
-
-g=nx.read_graphml('Datasets/DD/DD_1.graphml')
-#na=NeighAssemb(g,'n1',500)
-#ng=NormalizeGraph(g, na, random_order_labeling, 500)
-#f=ReceptiveField(g,random_order_labeling,'n1',100)
-ns=SelNodeSeq(g,random_order_labeling,stride=3,width=10,k=100)
-p=0
-'''
+def is_canonical(nodes_lbls_prev, nodes_lbls):
+    return all([nodes_lbls[k][0] == nodes_lbls_prev[k][0] for k in nodes_lbls.keys()])
